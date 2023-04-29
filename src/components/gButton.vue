110e5e7724b6f9e4599fcf96c7836aa2c9058927
yuan <11297393+yingeran@user.noreply.gitee.com>
2023-04-29 22:58:58 +0800

    提交readme

---
+++ src/components/gButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button>按钮</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "gButton",
+};
+</script>
+
+<style scoped lang="less">
+button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+}
+
+button:hover {
+  border-color: var(--border-color-hover);
+}
+
+// 点击后显示的背景色
+button:active {
+  background-color: var(--button-active-bg);
+}
+
+// button:focus {
+//   outline: none;
+// }  
+</style>s
